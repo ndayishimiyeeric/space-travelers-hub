9657a6940786477daf0833700d032b28f56a38fc
David Abol Ger <draker74@gmail.com>
2022-06-14 00:42:27 +0300

    start push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
+
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+
+    </>
   );
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './styles/Header.module.css';
+import logo from './images/logo.png';
 
 function Header() {
   return (
-    <div>This is the Header</div>
+    <nav className={styles.navbar}>
+      <div className={styles.logodiv}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </div>
+      <ul className={styles.links}>
+        <li><Link to="/">Rockets</Link></li>
+        <li><Link to="/Missions">Missions</Link></li>
+        <li><Link to="/Profile">My Profile</Link></li>
+      </ul>
+    </nav>
   );
 }
 
