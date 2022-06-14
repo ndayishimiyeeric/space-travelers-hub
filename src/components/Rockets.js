62ffde5f6c28c45ab158f3c7311bc5c4716bae24
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-14 17:53:38 +0200

    Add rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Rocket from './Rocket';
+import styles from './styles/Rockets.module.css';
 
 function Rockets() {
+  const rockets = useSelector((state) => state.rockets);
+  const rocketElements = rockets.map((rocket) => (
+    <Rocket
+      key={rocket.id}
+      id={rocket.id}
+      img={rocket.img}
+      description={rocket.description}
+      title={rocket.rocket_name}
+      active={rocket.active}
+    />
+  ));
   return (
-    <div>Rockets</div>
+    <div className={styles.rocketsDiv}>
+      { rocketElements }
+    </div>
   );
 }
 
