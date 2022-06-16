2a7f0289100c0c62f8d3d72602a5816c24f76dcc
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-16 20:05:39 +0200

    final touches

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,29 +1,52 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import Mission from './Mission';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserve } from '../redux/missions/missions';
+// import Mission from './Mission';
 import styles from './styles/Missions.module.css';
 
 const Missions = () => {
   const missions = useSelector((state) => state.mission);
+  const dispatch = useDispatch();
+  const handleReserve = (id) => {
+    dispatch(reserve(id));
+  };
   const missionElements = missions.map((mission) => (
-    <Mission
-      key={mission.id}
-      id={mission.id}
-      missionName={mission.mission_name}
-      description={mission.description}
-      active={mission.active}
-    />
+    <tr key={mission.id}>
+      <td className={styles.name}>{ mission.mission_name }</td>
+      <td className={styles.desc}>{ mission.description }</td>
+      <td className={styles.statusSpace}><span className={mission.active ? styles.cardTOpen : styles.cardT}>{ mission.active ? 'Active Member' : 'NOT A MEMBER' }</span></td>
+      <td className={styles.buttonSpace}>
+        <button
+          type="button"
+          className={mission.active ? styles.btnOpen : styles.btnClosed}
+          onClick={() => handleReserve(mission.id)}
+        >
+          { mission.active ? 'Leave Mission' : 'Join Mission' }
+        </button>
+      </td>
+    </tr>
   ));
 
   return (
-    <>
-      <p className={styles.header}>Mission Name</p>
-      <p className={styles.header}>Description</p>
-      <p className={styles.header}>Status</p>
-      <p className={styles.header}>Active</p>
+    <div className={styles.mainDiv}>
+      {/* <p className={styles.header}>Mission</p>
+      <p className={styles.desc}>Description</p>
+      <p className={styles.header}>Status</p> */}
+      <table>
+        <thead className={styles.headers}>
+          <tr>
+            <th className={styles.name}>Mission</th>
+            <th className={styles.desc}>Description</th>
+            <th className={styles.statusSpace}>Status</th>
+            <th className={styles.buttonSpace}>{}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missionElements}
+        </tbody>
+      </table>
 
-      { missionElements }
-    </>
+    </div>
   );
 };
 
